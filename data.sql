d57f079f13408e64efbea30e349fd2369bd97e5e
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-05-10 19:59:28 +0100

    Add data to animals table and save it to data.sql

---
+++ data.sql
@@ -0,0 +1,61 @@
+/* Populate database with sample data. */
+
+INSERT INTO 
+    animals (
+        id,
+        name,
+        date_of_birth,
+        weight_kg, neutered,
+        escape_attempts )
+    values(
+        1,
+        'Agumon',
+        'Feb 3, 2020',
+        10.23,
+        true,
+        0 );
+
+INSERT INTO 
+    animals (
+        id,
+        name,
+        date_of_birth,
+        weight_kg, neutered,
+        escape_attempts )
+    values(
+        2,
+        'Gabumon',
+        'Nov 15, 2018',
+        8,
+        true,
+        2 );
+
+INSERT INTO 
+    animals (
+        id,
+        name,
+        date_of_birth,
+        weight_kg, neutered,
+        escape_attempts )
+    values(
+        3,
+        'Pikachu',
+        'Jan 7, 2021',
+        15.04,
+        false,
+        1 );
+
+INSERT INTO 
+    animals (
+        id,
+        name,
+        date_of_birth,
+        weight_kg, neutered,
+        escape_attempts )
+    values(
+        1,
+        'Devimon',
+        'May 12, 2017',
+        11,
+        true,
+        5 );
