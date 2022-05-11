ab8025665e230c4de6fd572e72dab79d0833fd02
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-05-11 16:45:47 +0100

    Insert Data to animals TABLE

--- data.sql
+++ data.sql
@@ -53,9 +53,116 @@ INSERT INTO
         weight_kg, neutered,
         escape_attempts )
     values(
-        1,
+        4,
         'Devimon',
         'May 12, 2017',
         11,
         true,
         5 );
+
+    /* Query and update*/
+
+INSERT INTO animals (
+    id,
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts)
+VALUES (
+    5,
+    'Charmander',
+    'Feb 8, 2020',
+    -11,
+    false,
+    0);
+
+INSERT INTO animals (
+    id,
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts)
+VALUES (
+    6,
+    'Plantmon',
+    'Nov 15, 2021',
+    -5.7,
+    true,
+    2);
+
+INSERT INTO animals (
+    id,
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts)
+VALUES (
+    7,
+    'Squirtle',
+    'Apr 2, 1993',
+    -12.13,
+    false,
+    3);
+
+INSERT INTO animals (
+    id,
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts)
+VALUES (
+    8,
+    'Angemon',
+    'Jun 12, 2005',
+    -45,
+    true,
+    1);
+
+INSERT INTO animals (
+    id,
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts)
+VALUES (
+    9,
+    'Boarmon',
+    'Jun 7, 2005',
+    20.4,
+    true,
+    7);
+
+INSERT INTO animals (
+    id,
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts)
+VALUES (
+    10,
+    'Blossom',
+    'Oct 13, 1998',
+    17,
+    true,
+    3);
+
+INSERT INTO animals (
+    id,
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts)
+VALUES (
+    11,
+    'Ditto',
+    'May 14, 2022',
+    22,
+    true,
+    4);
