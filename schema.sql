e6ccc2dcd3c78e83a1113021f171ccddd9623705
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-05-12 17:47:01 +0100

    Create schema and update schema.sql

--- schema.sql
+++ schema.sql
@@ -39,3 +39,42 @@ ADD COLUMN owner_id INT,
 ADD FOREIGN KEY (owner_id) 
 REFERENCES owners (id) 
 ON DELETE CASCADE;
+
+CREATE TABLE vets(
+  id INT GENERATED ALWAYS AS IDENTITY, 
+  name VARCHAR(255), 
+  age INT, 
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
+  );
+
+CREATE TABLE specialization(
+  vets_id INT, 
+  species_id INT);
+
+ALTER TABLE specialization
+ADD FOREIGN KEY (vets_id) 
+REFERENCES vets (id) 
+ON DELETE CASCADE;
+
+ALTER TABLE specialization 
+ADD FOREIGN KEY (species_id) 
+REFERENCES species (id) 
+ON DELETE CASCADE;
+
+CREATE TABLE visits(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT, 
+  vet_id INT, 
+  date_of_vist DATE
+  );
+
+ALTER TABLE visits 
+ADD FOREIGN KEY (animal_id) 
+REFERENCES animals (id) 
+ON DELETE CASCADE;
+
+ALTER TABLE visits 
+ADD FOREIGN KEY (vet_id) 
+REFERENCES vets (id) 
+ON DELETE CASCADE;
